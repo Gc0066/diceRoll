4caf7bfe92373bc4b207d3f8f92268a376b58573
George Coleman <colemang@ecm15-mac-14.coventry.ac.uk>
2018-10-11 10:46:03 +0100

    Finished dice roll lab

--- diceRoll/ViewController.swift
+++ diceRoll/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var leftImage: UIImageView!
+    
+    
+    @IBOutlet weak var rightImage: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let numberOne = arc4random_uniform(6) + 1
+        let numberTwo = arc4random_uniform(6) + 1
 
+        label.text = "The sum is: \(numberOne + numberTwo)"
+
+        leftImage.image = UIImage(named: "Dice\(numberOne)")
+        rightImage.image = UIImage(named: "Dice\(numberTwo)")
+        
+    }
+    
 }
 
