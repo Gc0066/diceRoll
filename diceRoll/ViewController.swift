4ff400e90969969dab407ff2db70928926df6dca
George Coleman <colemang@ecm15-mac-14.coventry.ac.uk>
2018-10-11 09:24:27 +0100

    Initial Commit

---
+++ diceRoll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  diceRoll
+//
+//  Created by George Coleman on 11/10/2018.
+//  Copyright © 2018 George Coleman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
